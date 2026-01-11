b6e0e43b983b08c6545b0f28ad1e78ec2b954997
Honton129 <85850584+Honton129@users.noreply.github.com>
2026-01-11 17:44:00 -0300

    Override parseModule for different hscript versions

--- src/hscript/ParserEx.hx
+++ src/hscript/ParserEx.hx
@@ -1,8 +1,15 @@
 package hscript;
 
 class ParserEx extends Parser {
+    #if (hscript >= "2.7.0")
+    public override function parseModule( content : String, ?origin : String = "hscript", ?position = 0) {
+        var decls = super.parseModule(content, origin, position);
+        return decls;
+    }
+    #else
     public override function parseModule( content : String, ?origin : String = "hscript" ) {
         var decls = super.parseModule(content, origin);
         return decls;
     }
+    #end
 }
